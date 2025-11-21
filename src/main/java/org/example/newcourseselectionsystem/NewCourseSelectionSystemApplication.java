232af09fb832c1a6bef94add82d81ee019f78af5
chenzhan231098166 <231098166@smail.nju.edu.cn>
2025-11-21 23:54:19 +0800

    first commit

---
+++ src/main/java/org/example/newcourseselectionsystem/NewCourseSelectionSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.newcourseselectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCourseSelectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewCourseSelectionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/newcourseselectionsystem/NewCourseSelectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newcourseselectionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCourseSelectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
